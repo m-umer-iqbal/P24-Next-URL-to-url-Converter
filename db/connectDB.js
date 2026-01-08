b5246955aa1b4f4b4a467e1b485a5f9c8a0c4d01
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-01-08 10:04:00 +0500

    Add database connection utility and constants for MongoDB integration

---
+++ db/connectDB.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+let isConnected = false;
+
+const connectDB = async () => {
+    if (isConnected) return;
+
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URL}/${DB_NAME}`
+        );
+
+        isConnected = true;
+
+        console.log(
+            `MongoDB connected\nDB Host: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
